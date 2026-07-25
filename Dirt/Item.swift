ce2ea1ca7d39a4d49fc612c76da5dcc57e78c161
HellaCreative <62033648+HellaCreative@users.noreply.github.com>
2026-07-25 09:06:06 -0300

    Initial Commit

---
+++ Dirt/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Dirt
+//
+//  Created by Richard Smith on 7/25/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ DirtTests/DirtTests.swift
@@ -0,0 +1,19 @@
+//
+//  DirtTests.swift
+//  DirtTests
+//
+//  Created by Richard Smith on 7/25/26.
+//
+
+import Testing
+@testable import Dirt
+
+struct DirtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
